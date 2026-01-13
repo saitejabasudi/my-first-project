67a3035879f70bee185aca0a6f1e010d35deeff7
Saiteja Basudi <saitejabasudi2426@gmail.com>
2026-01-13 11:35:46 +0000

    Fix errors

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -14,8 +14,8 @@ export function Logo({ className }: { className?: string }) {
       strokeLinejoin="round"
       className={cn('h-6 w-6', className)}
     >
-      <path d="m16 18 6-6-6-6" />
-      <path d="m8 6-6 6 6 6" />
+        <path d="m16 18 6-6-6-6" />
+        <path d="m8 6-6 6 6 6" />
     </svg>
   );
 }
